4bd94787ba08933295ef5dbc75cde875bbdedc00
moham <mohammadhoseinnoori876@gmail.com>
2026-07-16 21:04:08 +0330

    sidebar completed

--- frontend/src/Layouts/Sidebar.jsx
+++ frontend/src/Layouts/Sidebar.jsx
@@ -1,10 +1,16 @@
+import dashboard from '../assets/icons/dashboard.svg';
+import order from '../assets/icons/order.svg';
+import product from '../assets/icons/product.svg';
+import user from '../assets/icons/user.svg';
+import mainIcon from '../assets/icons/mainIcon.svg';
 export default function Sidebar() {
+    console.log(dashboard);
     return (
-        <aside className="w-64 min-h-screen bg-white flex flex-col shadow-black shadow-md">
+        <aside className="w-80 min-h-screen bg-white flex flex-col shadow-black shadow-md">
 
             <div className="flex items-center gap-3 p-6">
-                <div className="w-12 h-12 rounded-lg bg-gray-200 flex items-center justify-center">
-                    Icon
+                <div className="w-12 h-12 rounded-lg bg-(--bg-secondary) flex items-center justify-center">
+                    <img className="size-8 " src={mainIcon} alt=""/>
                 </div>
 
                 <div>
@@ -21,25 +27,22 @@ export default function Sidebar() {
             <nav className="flex-1 p-4 space-y-2">
 
                 <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-(--bg-secondary) text-(--text) font-bold hover:text-white transition">
-                    <span>📊</span>
-                    <span className=" ">داشبورد</span>
+                    <span className="size-5"><img src={dashboard} alt=""/></span>
+                    <span className="">داشبورد</span>
                 </button>
                 <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-(--bg-secondary) text-(--text) font-bold hover:text-white transition">
-                    <span>📊</span>
-                    <span className=" ">داشبورد</span>
+                    <span className="size-5"><img src={order} alt=""/></span>
+                    <span className="">سفارشات</span>
                 </button>
                 <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-(--bg-secondary) text-(--text) font-bold hover:text-white transition">
-                    <span>📊</span>
-                    <span className=" ">داشبورد</span>
+                    <span className="size-5"><img src={product} alt=""/></span>
+                    <span className="">محصولات</span>
                 </button>
                 <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-(--bg-secondary) text-(--text) font-bold hover:text-white transition">
-                    <span>📊</span>
-                    <span className=" ">داشبورد</span>
+                    <span className="size-5"><img src={user} alt=""/></span>
+                    <span className="">مشتری ها</span>
                 </button>
-                <button className="w-full flex items-center gap-3 px-4 py-3 rounded-lg hover:bg-(--bg-secondary) text-(--text) font-bold hover:text-white transition">
-                    <span>📊</span>
-                    <span className=" ">داشبورد</span>
-                </button>
+
 
             </nav>
 
--- frontend/src/routes/appRoutes.jsx
+++ frontend/src/routes/appRoutes.jsx
@@ -2,5 +2,5 @@ import {createBrowserRouter} from "react-router";
 import Sidebar from "../Layouts/Sidebar.jsx";
 
 export const routes = createBrowserRouter([
-    { path: '/', element: <Sidebar/>},
+    { path: '/', element: <Sidebar/>}
 ])

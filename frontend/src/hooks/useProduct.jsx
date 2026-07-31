d5b8a6439cdcb70f3b457e5e6c39bad92d10adc8
moham <mohammadhoseinnoori876@gmail.com>
2026-07-31 11:13:42 +0330

    custom hooks for product requests

---
+++ frontend/src/hooks/useProduct.jsx
@@ -0,0 +1,51 @@
+import {useMutation, useQuery} from "@tanstack/react-query";
+import {createProduct, deleteProduct, editProduct, getProduct, getProducts,} from "../api/productApi.jsx";
+import {queryClient} from "../main.jsx";
+
+export function useProducts() {
+    return useQuery({
+        queryKey: ["products"],
+        queryFn: getProducts,
+    });
+}
+
+export function useProduct(id) {
+    return useQuery({
+        queryKey: ["products", id],
+        queryFn: () => getProduct(id),
+        enabled: !!id,
+    });
+}
+
+export function useCreateProduct() {
+    return useMutation({
+        mutationFn: createProduct,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["products"],
+            });
+        },
+    });
+}
+
+export function useEditProduct() {
+    return useMutation({
+        mutationFn: ({id, product}) => editProduct(id, product),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["products"],
+            });
+        },
+    });
+}
+
+export function useDeleteProduct() {
+    return useMutation({
+        mutationFn: (id) => deleteProduct(id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["products"],
+            });
+        },
+    });
+}
